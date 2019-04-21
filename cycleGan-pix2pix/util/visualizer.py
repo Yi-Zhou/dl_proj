c83a924fda8eb6b2a4813a9a9939ccb45441e95b
deltadu <jianlind@andrew.cmu.edu>
2019-04-21 03:19:53 -0400

    little bugs fixed, added sys.stdout.flush() to better run in background
    self.real_A is of shape (1, 4, 256, 256) instead of (4, 256, 256)

--- cycleGan-pix2pix/util/visualizer.py
+++ cycleGan-pix2pix/util/visualizer.py
@@ -223,5 +223,6 @@ class Visualizer():
             message += '%s: %.3f ' % (k, v)
 
         print(message)  # print the message
+        sys.stdout.flush()
         with open(self.log_name, "a") as log_file:
             log_file.write('%s\n' % message)  # save the message
